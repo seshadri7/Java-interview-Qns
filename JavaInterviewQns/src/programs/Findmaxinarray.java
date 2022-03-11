4b5c0f457fceb38121b5bba5c90bef7eab4fa755
Seshadri Vishwanatham <seshadrivishwanatham@Seshadris-MacBook-Pro.local>
2022-03-11 13:41:33 +0530

    Updated find maximum number in array class

--- JavaInterviewQns/src/programs/Findmaxinarray.java
+++ JavaInterviewQns/src/programs/Findmaxinarray.java
@@ -1,23 +1,25 @@
 package programs;
 
 public class Findmaxinarray {
-	
-public static 	int a[] = {5,6,3,7,9,8};
- public static  int b= a.length; // 6
-	
-public static int max = a[0];
+
+	public static int a[] = { 5, 6, 3, 7, 9, 8 };
+	public static int b = a.length;
+
+	//taking first index in array
+	public static int max = a[0];
 
 	public static void main(String[] args) {
-		//System.out.println("Sample ==== >"+max);
-		for(int i=0;i<b;i++) { 
+
+		for (int i = 0; i < b; i++) {
+
+			//Comparing to all values based on the first index value
 			
-			//System.out.println("samp 2 "+ max);
-			if(max<a[i]) {
-			max = a[i];
+			if (max < a[i]) {
+				max = a[i];
 			}
-			
+
 		}
-		System.out.println(max);
+		System.out.println("Maximum number is: "+ max);
 
 	}
 
