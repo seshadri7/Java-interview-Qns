5e65b1192d298f6af043930122f2d3cfca5f940b
Seshadri Vishwanatham <seshadrivishwanatham@Seshadris-MacBook-Pro.local>
2022-03-23 10:39:41 +0530

    Switchcase added

---
+++ JavaInterviewQns/src/programs/SwitchCase.java
@@ -0,0 +1,26 @@
+package programs;
+
+public class SwitchCase {
+
+	
+	public static void main(String[] args) {
+		
+		int i=2;
+		
+		
+		
+		switch(i) {
+		
+		case 1:
+			System.out.println("This is case1 value");
+			
+		case 2:
+			System.out.println("This is case2 value");
+			
+		case 3:
+			default:
+				System.out.println("This is a default value");
+			
+		}
+	}
+}
